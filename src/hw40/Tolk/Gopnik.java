36c88053a773824c2e17c22bde6c6b71d21a03ee
Kate Horshkova <poligrafist87@gmail.com>
2023-05-11 20:32:53 +0200

    hw interface Dialog

---
+++ src/hw40/Tolk/Gopnik.java
@@ -0,0 +1,59 @@
+package hw40.Tolk;
+
+public class Gopnik {
+
+    private String clothes;
+    private String cap;
+    private String cigarette;
+    private int age;
+    private int friends;
+    private String language;
+
+    public String getClothes() {
+        return clothes;
+    }
+
+    public String getCap() {
+        return cap;
+    }
+
+    public String getCigarette() {
+        return cigarette;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getFriends() {
+        return friends;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setClothes(String clothes) {
+        this.clothes = clothes;
+    }
+
+    public void setCap(String cap) {
+        this.cap = cap;
+    }
+
+    public void setCigarette(String cigarette) {
+        this.cigarette = cigarette;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFriends(int friends) {
+        this.friends = friends;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
---
+++ src/hw40/Tolk/Professor.java
@@ -0,0 +1,9 @@
+package hw40.Tolk;
+
+public class Professor {
+
+
+
+
+
+}
