b84ac3119860e02f1054352752052127aee28380
Kate Horshkova <poligrafist87@gmail.com>
2023-05-17 19:41:45 +0200

    hw new class for test Pizza.java

---
+++ src/hw44/Pizza.java
@@ -0,0 +1,15 @@
+package hw44;
+
+import java.util.Scanner;
+
+public class Pizza {
+    public static Pizza readPizza(Scanner scanner){
+        return new Pizza();
+    }
+    public double getPrice(){
+        return 0.0;
+    }
+    public String toString(){
+        return " ";
+    }
+}
