1ad72db5702c0700f0f86b506718649ce9237700
Kate Horshkova <poligrafist87@gmail.com>
2023-05-17 18:59:45 +0200

    hw new class for test

---
+++ src/hw44/Order.java
@@ -0,0 +1,15 @@
+package hw44;
+
+import java.util.Scanner;
+
+public class Order {
+    public void addPizza(Scanner scanner){
+
+    }
+    public boolean ifEmpty(){
+        return false;
+    }
+    public double getTotal(){
+        return 0.0;
+    }
+}
