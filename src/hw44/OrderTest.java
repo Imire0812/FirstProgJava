48ce0a9be45605ed6cb718075477e2859795fd11
Kate Horshkova <poligrafist87@gmail.com>
2023-05-17 21:26:10 +0200

    hw add ready class test

--- src/hw44/OrderTest.java
+++ src/hw44/OrderTest.java
@@ -1,14 +1,16 @@
 package hw44;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class OrderTest {
+
     @Test
-    public void testOrderNumber(){
+    public void testOrderNumber() {
         Order order = new Order();
         assertEquals(1, order.getNumber());
-        order = new Order();
+        Order order2 = new Order();
+        assertEquals(1, order.getNumber());
         assertEquals(2, order2.getNumber());
     }
 }
