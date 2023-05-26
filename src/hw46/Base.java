0f5a1b4a4cc3694ad207fd272f07aa06c0e8c58b
Kate Horshkova <poligrafist87@gmail.com>
2023-05-26 20:42:58 +0200

    hw add class

--- src/hw46/Base.java
+++ src/hw46/Base.java
@@ -5,8 +5,17 @@ import java.text.ParseException;
 import java.util.*;
 
 public class Base {
+    // Будет общая база (система учёта), в которой заказы будут храниться в Map<Integer, Order>
+    // Заказы могут удаляться, а ID при этом меняться не будут. Нумерация будет "разреженной" 1, 2, 7
     private final Map<Integer, Order> orders = new HashMap<>();
 
+    // Внутренняя система учёта заказов в цветочном магазине.
+    // Функционал:
+    // - добавить заказ
+    // - изменить заказ
+    // - удалить заказ
+
+    // добавить заказ: работает с клавиатурой
     public void add() {
         Scanner scanner = new Scanner(System.in);
         boolean added = false; // флаг "заказ добавлен"
@@ -14,7 +23,7 @@ public class Base {
         String content = scanner.nextLine();
         do {
             try {
-                System.out.print("Введите дату (в формате 'dd.MM.yyyy HH:mm': ");
+                System.out.print("Введите дату (в формате 'dd.MM.yyyy HH:mm'): ");
                 String date = scanner.nextLine();
                 add(content, date); // работает с прочитанными данными
                 // если не случилось ошибки, то
@@ -25,6 +34,7 @@ public class Base {
         } while (!added); // повторять, пока заказ не добавлен
     }
 
+    // добавить заказ: работает с готовыми данными
     public void add(String content, String date) throws ParseException {
         Order order = new Order(content, date);
         int orderId = order.getId();
@@ -35,9 +45,18 @@ public class Base {
         }
         orders.put(orderId, order);
     }
-    public void printOrders(){
+
+    public void printOrders() {
+        // перебор словаря (Map):
+//    for (Map.Entry<Integer, Order> entry : orders.entrySet()) { // перебор пар ключ-значение
+//    for (int orderId : orders.keySet()) { // перебор ключей
+//    for (Order order : orders.values()) { // перебор значений
+        // Map<Integer, Order> orders - словарь с ID и заказами
+        // orders.values() - Collection<Order> - все значения словаря - набор заказов
+        // new ArrayList(...) - создать список из указанной в скобках коллекции
+        // итог: List<Order> orders - список заказов (чтобы можно было сортировать)
         List<Order> orders = new ArrayList<>(this.orders.values());
-        for (Order order : orders){
+        for (Order order : orders) {
             System.out.println(order);
         }
     }
--- src/hw46/Main.java
+++ src/hw46/Main.java
@@ -2,6 +2,9 @@ package hw46;
 
 public class Main {
     public static void main(String[] args) {
-
+        Base orders = new Base();
+        orders.add();
+        orders.printOrders();
     }
 }
+
