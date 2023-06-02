a5363a20943b0a2e1cf693e716e477be00d08547
Kate Horshkova <poligrafist87@gmail.com>
2023-06-02 19:54:49 +0200

    add PreRLE method

--- src/hw53/PreRLE.java
+++ src/hw53/PreRLE.java
@@ -7,13 +7,20 @@ public class PreRLE {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку для сжатия: ");
         String line = scanner.nextLine();
-        String result = preRle(Line);
+        String result = preRle(line);
         System.out.println("Промежуточный результат сжатия: " + result);
         System.out.println(result);
     }
-    public static String preRle(String line){
+    public static String preRle(String line) {
         String result = "";
-
+        Character prev = null; // никакого "предыдущего" ещё нет
+        for (int i = 0; i < line.length(); ++i) {
+            Character current = line.charAt(i);
+            if (!current.equals(prev)) {
+                result += current;
+                prev = current;
+            }
+        }
         return result;
     }
 }
