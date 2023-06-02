89a4483f9458d94c106aced01503ad5c75fbc953
Kate Horshkova <poligrafist87@gmail.com>
2023-06-02 19:47:25 +0200

    add PreRLE temp

---
+++ src/hw53/PreRLE.java
@@ -0,0 +1,19 @@
+package hw53;
+
+import java.util.Scanner;
+
+public class PreRLE {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку для сжатия: ");
+        String line = scanner.nextLine();
+        String result = preRle(Line);
+        System.out.println("Промежуточный результат сжатия: " + result);
+        System.out.println(result);
+    }
+    public static String preRle(String line){
+        String result = "";
+
+        return result;
+    }
+}
